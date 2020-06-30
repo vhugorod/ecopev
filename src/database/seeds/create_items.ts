c001526e5d1a608c4c85d83db223c8664a33cd16
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2020-06-30 00:30:12 -0300

    new items

--- src/database/seeds/create_items.ts
+++ src/database/seeds/create_items.ts
@@ -2,11 +2,21 @@ import Knex from 'knex';
 
 export async function seed(knex: Knex) {
   await knex('items').insert([
-    { title: 'Lâmpadas', image: 'lampadas.svg' },
-    { title: 'Pilhas e Baterias', image: 'baterias.svg' },
-    { title: 'Papéis e Papelão', image: 'papeis-papelao.svg' },
+    { title: 'Papéis', image: 'papeis.svg' },
+    { title: 'Metal', image: 'metal.svg' },
     { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
-    { title: 'Resíduos Orgânicos', image: 'organicos.svg' },
+    { title: 'Plástico', image: 'plastico.svg' },
+    { title: 'Vidro', image: 'vidro.svg' },
+    { title: 'Pilhas e Baterias', image: 'baterias.svg' },
+    { title: 'Lâmpadas', image: 'lampadas.svg' },
     { title: 'Óleo de Cozinha', image: 'oleo.svg' },
+    { title: 'Esponja', image: 'esponja.svg' },
+    { title: 'Alumínio', image: 'aluminio.svg' },
+    { title: 'Papelão', image: 'papelao.svg' },
+    { title: 'Garrafa Pet', image: 'garrafa.svg' },
+    { title: 'Material de Construção', image: 'material-construcao.svg' },
+    { title: 'Poda de Jardinagem', image: 'poda-jardinagem.svg' },
+    { title: 'Móveis', image: 'moveis.svg' },
+    { title: 'Reciclados Comuns', image: 'reciclados-comuns.svg' },
   ]);
 }
